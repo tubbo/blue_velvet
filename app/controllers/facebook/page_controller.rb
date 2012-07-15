73cc8a07b1b2558e92a7dc7e1ee2f31adc2f4eb5
Tom Scott <tubbo@psychedeli.ca>
2012-07-15 18:39:01 -0400

    Ignore sass cache, add facebook namespace to assets, document Facebook::PageController

--- app/controllers/facebook/page_controller.rb
+++ app/controllers/facebook/page_controller.rb
@@ -1,7 +1,29 @@
-# Grabs attributes from the Facebook page and displays them. You may optionally define custom actions
-# here which get multiple data sources.
+# The interim between the Facebook page data and the end user, this defines the internal API
+# used by the Rails app to call Facebook attributes as if they were controller actions.
 class Facebook::PageController < ApplicationController
-  # Gets any attribute from the Facebook page and displays it in the page_attribute template.
+  # Much like method_missing, this method is called when ApplicationController can't find
+  # the hard-coded action. For this controller specifically, it will be called on all routing
+  # requests to +facebook/page+.
+  #
+  # This method retrieves any attribute from the Facebook page and displays it with a consistent
+  # partial template. The template used is page_attribute, which creates a +<div class="facebook_attribute"
+  # id="{whatever-facebook-attribute-you-used}>+ and renders it with the contents of the Facebook
+  # attribute. Because this method makes use of a partial, it can be called in place of any other
+  # +render+ or +redirect_to+ call, from any part of the application like so:
+  # 
+  #     class InfoController < ApplicationController
+  #       def about
+  #         render 'facebook/page#description'
+  #       end
+  #     end
+  #
+  # Or, you may route to the action:
+  #
+  #     get "/about" => "facebook/page#description"
+  #
+  # Note that using routes.rb is easier, but binds you to using CSS as the main method of
+  # customization. You may override our styles if you simply create styles for
+  # +.facebook_attribute+, and for individual pages the name of the attribute as an ID.
   def action_missing attribute_name
     if facebook.has_attribute? attribute_name
       attribute_content = facebook.send(:"#{attribute_name}").gsub(/\n/, "<br>")
@@ -15,13 +37,13 @@ class Facebook::PageController < ApplicationController
     end
   end
 
-  protected
-
+protected
   # Shorthand accessor for the omnipresent Facebook client
   def facebook
     @facebook_client ||= Facebook::Client.new
   end
 
+private
   def use_layout
     if request.xhr?
       false
--- lib/blue_velvet/engine.rb
+++ lib/blue_velvet/engine.rb
@@ -1,5 +1,6 @@
 module BlueVelvet
   class Engine < ::Rails::Engine
     isolate_namespace BlueVelvet
+    isolate_namespace Facebook
   end
 end
