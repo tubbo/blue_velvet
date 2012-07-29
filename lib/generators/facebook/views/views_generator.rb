0864ac3bd38065aca40a176b0b4b489eb83e13b4
Tom Scott <tubbo@psychedeli.ca>
2012-07-29 00:37:02 -0400

    Refactor generators to use Facebook namespace

--- lib/generators/facebook/views/views_generator.rb
+++ lib/generators/facebook/views/views_generator.rb
@@ -1,16 +1,10 @@
-module BlueVelvet
-  module Generators
-    class ViewsGenerator < Rails::Generators::NamedBase
-      source_root File.expand_path('../templates', __FILE__)
+module Facebook
+  # Generate views for the Facebook namespace.
+  class ViewsGenerator < Rails::Generators::NamedBase
+    source_root File.expand_path('../../../../../app/views', __FILE__)
 
-      def copy_page_views
-        views_dir = File.join(__FILE__, 'app', 'views', 'facebook')
-        Dir[views_dir].each do |view_file_path|
-          view_file = File.basename view_file_path
-          app_path = "#{Rails.root}/app/views/facebook/#{view_file}"
-          copy_file view_file_path, app_path
-        end
-      end
+    def copy_views
+      directory "facebook", "app/views/facebook"
     end
   end
 end
