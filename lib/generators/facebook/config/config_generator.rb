9a3f10c6ccee0953b05def83715e126119a72416
Tom Scott <tubbo@psychedeli.ca>
2012-07-29 00:27:29 -0400

    Move everything to the Facebook namespace for generators

---
+++ lib/generators/facebook/config/config_generator.rb
@@ -0,0 +1,15 @@
+module BlueVelvet
+  module Generators
+    class ConfigGenerator < Rails::Generators::NamedBase
+      desc "Copies Facebook/Koala configuration to your application"
+      source_root File.expand_path('../templates', __FILE__)
+
+      # Edit this file with your Facebook app details
+      def copy_configuration
+        copy_file "facebook.yml", "config/facebook.yml"
+        copy_file "koala.rb",     "config/initializers/koala.rb"
+      end
+    end
+  end
+end
+
