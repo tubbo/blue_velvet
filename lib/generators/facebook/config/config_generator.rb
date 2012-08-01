c78c200c48dd3add4962fc41cd09bfd37d2d494d
Tom Scott <tubbo@psychedeli.ca>
2012-07-31 22:33:04 -0400

    Fix stack overflow issue.
    Generating the Koala initializer as an override is causing problems,
    caused by a misunderstanding of how initializers are loaded in a
    Rails engine. Now that it's fixed, the generator should no longer
    cause stack overflow errors on invocation.
    
    - Remove koala.rb instruction from facebook:config generator
    - Remove koala.rb from templates directory
    - Bump version to v1.0.2 and publish.

--- lib/generators/facebook/config/config_generator.rb
+++ lib/generators/facebook/config/config_generator.rb
@@ -1,13 +1,12 @@
 module Facebook
   # Generate configuration for BlueVelvet and Koala.
   class ConfigGenerator < Rails::Generators::Base
-    desc "Copies Facebook/Koala configuration to your application"
+    desc "Copies Facebook configuration to your application"
     source_root File.expand_path('../templates', __FILE__)
 
     # Edit this file with your Facebook app details
     def copy_configuration
       copy_file "facebook.yml", "config/facebook.yml"
-      copy_file "koala.rb",     "config/initializers/koala.rb"
     end
   end
 end
